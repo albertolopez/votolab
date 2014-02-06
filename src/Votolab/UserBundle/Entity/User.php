b2ac37c8c8bc404957ace549f2b1ec025c603ef4
Alberto López <x@albertolopez.cc>
2014-02-06 01:59:11 +0100

    Initial commit

---
+++ src/Votolab/UserBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+// src/Votolab/UserBundle/Entity/User.php
+
+namespace Votolab\UserBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="votolab_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
