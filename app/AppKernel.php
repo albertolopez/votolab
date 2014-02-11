7dee9949812ad459d461187faae484b177103783
César Rodríguez <kesarr@gmail.com>
2014-02-11 19:02:04 +0100

    FOSJsRoutingBundle support

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Votolab\VotolabBundle\VotolabBundle(),
             new Votolab\RCPXBundle\RCPXBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
