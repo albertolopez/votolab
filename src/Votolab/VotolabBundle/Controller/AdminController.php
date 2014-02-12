1d9f98402ad7c2ab2d46cca7d6711f28be803773
César Rodríguez <kesarr@gmail.com>
2014-02-12 23:33:19 +0100

    Election Forms Admin

--- src/Votolab/VotolabBundle/Controller/AdminController.php
+++ src/Votolab/VotolabBundle/Controller/AdminController.php
@@ -4,7 +4,11 @@ namespace Votolab\VotolabBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
 use Votolab\VotolabBundle\Entity\Election;
+use Votolab\VotolabBundle\Form\Handler\ElectionFormHandler;
+use Votolab\VotolabBundle\Form\Model\ElectionFormClass;
+use Symfony\Component\Form\Form;
 
 class AdminController extends Controller
 {
@@ -22,27 +26,52 @@ class AdminController extends Controller
     /**
      * @template
      */
-    public function addElectionsAction()
+    public function addElectionsAction(Request $request)
     {
-        return array();
+        $election = new Election();
+        return $this->createOrEditProject($request, $election);
     }
 
     /**
      * @template
      */
-    public function editElectionsAction(Election $election)
+    public function editElectionsAction(Request $request, Election $election)
     {
-        return array('election' => $election);
+        return $this->createOrEditProject($request, $election);
     }
 
     /**
-     * @template
+     * @param Request $request
+     * @param Election $election
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
+    private function createOrEditProject(Request $request, $election)
+    {
+        $form = $this->createForm('election', new ElectionFormClass($election));
+        if ($request->getMethod() == "POST") {
+            $formHandler = new ElectionFormHandler($form, $request, $this->get('election_manager'));
+            if ($formHandler->process()) {
+                $this->get('session')->getFlashBag()->set(
+                    'notice',
+                    "La elección <b>{$form->getData()->title}</b> ha sido creada"
+                );
+                return $this->redirect($this->generateUrl('votolab_admin'));
+            }
+        }
+
+        return $this->render(
+            'VotolabBundle:Admin:addElections.html.twig',
+            array(
+                'election' => $election,
+                'form' => $form->createView(),
+            )
+        );
+    }
+
     public function deleteElectionsAction(Election $election)
     {
         $electionsManager = $this->get('election_manager');
         $electionsManager->removeElection($election);
         $this->redirect($this->generateUrl('votolab_admin'));
     }
-
 }
---
+++ src/Votolab/VotolabBundle/Form/Model/ElectionFormClass.php
@@ -0,0 +1,72 @@
+<?php
+namespace Votolab\VotolabBundle\Form\Model;
+
+use Votolab\VotolabBundle\Entity\Election;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class ElectionFormClass
+{
+    /**
+     * @var integer
+     */
+    public $id;
+    /**
+     * @Assert\NotBlank(message="Título es obligatorio")
+     * @var string
+     */
+    public $title;
+    /**
+     * @Assert\NotBlank(message="Introduce una descripción")
+     * @var string
+     */
+    public $description;
+    /**
+     * @var string
+     * @Assert\NotBlank(message="Introduce una url")
+     */
+    public $slug;
+    /**
+     * @var \DateTime
+     */
+    public $dateStart;
+
+    /**
+     * @var \DateTime
+     */
+    public $dateEnd;
+
+    /**
+     * @var \DateTime
+     */
+    public $datePublished;
+
+    /**
+     * @var integer
+     */
+    public $minCandidates = 1;
+
+    /**
+     * @var integer
+     */
+    public $maxCandidates = 999;
+
+    /**
+     * @var boolean
+     */
+    public $publishResults;
+
+
+    public function __construct(Election $election)
+    {
+        $this->id = $election->getId();
+        $this->title = $election->getTitle();
+        $this->description = $election->getDescription();
+        $this->slug = $election->getSlug();
+        $this->dateStart = $election->getDateStart();
+        $this->dateEnd = $election->getDateEnd();
+        $this->minCandidates = $election->getMinCandidates();
+        $this->maxCandidates = $election->getMaxCandidates();
+        $this->publishResults = $election->getPublishResults();
+        $this->datePublished = $election->getDatePublished();
+    }
+}
