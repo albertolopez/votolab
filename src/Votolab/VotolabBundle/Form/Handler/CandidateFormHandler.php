b6b8857818298efc3c77add3055d99d5224a1c8f
César Rodríguez <kesarr@gmail.com>
2014-02-14 00:50:01 +0100

    form candidate + fixes entities

---
+++ src/Votolab/VotolabBundle/Form/Handler/CandidateFormHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Votolab\VotolabBundle\Form\Handler;
+
+use Votolab\VotolabBundle\Model\CandidateManager;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+
+class CandidateFormHandler
+{
+    protected $form;
+
+    protected $request;
+
+    protected $em;
+
+    public function __construct(FormInterface $form, Request $request, CandidateManager $manager)
+    {
+        $this->form = $form;
+        $this->request = $request;
+        $this->manager = $manager;
+    }
+
+    public function process()
+    {
+        $this->form->handleRequest($this->request);
+        if ($this->form->isValid()) {
+            return $this->manager->create($this->form->getData());
+        }
+        return false;
+    }
+}
--- src/Votolab/VotolabBundle/Model/CandidateManager.php
+++ src/Votolab/VotolabBundle/Model/CandidateManager.php
@@ -3,6 +3,7 @@
 namespace Votolab\VotolabBundle\Model;
 
 use Votolab\VotolabBundle\Entity\Candidate;
+use Votolab\VotolabBundle\Form\Model\CandidateFormClass;
 
 /**
  * candidate_manager
@@ -14,9 +15,32 @@ class CandidateManager extends ManagerAbstract
         return new Candidate();
     }
 
+    public function create(CandidateFormClass $electionFormClass)
+    {
+        if (is_numeric($electionFormClass->id)) {
+            $candidate = $this->em->getRepository('VotolabBundle:Candidate')->find($electionFormClass->id);
+        } else {
+            $candidate = new Candidate();
+        }
+        $candidate->setBiography($electionFormClass->biography);
+        $candidate->setGender($electionFormClass->gender);
+        $candidate->setName($electionFormClass->name);
+        $candidate->setVideo($electionFormClass->video);
+        $candidate->setElection($electionFormClass->election);
+        $this->em->persist($candidate);
+        $this->em->flush();
+        return true;
+    }
+
     public function persist(Candidate $candidate)
     {
         $this->em->persist($candidate);
+        $this->em->flush();
+    }
+
+    public function removeElection(Candidate $election)
+    {
+        $this->em->remove($election);
         $this->em->flush();
     }
 }
