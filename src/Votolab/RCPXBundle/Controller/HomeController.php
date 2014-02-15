230138d478811a3dc0c42d95f6bf15f2669e2bcf
César Rodríguez <kesarr@gmail.com>
2014-02-15 19:13:20 +0100

    refactoring

--- src/Votolab/RCPXBundle/Controller/HomeController.php
+++ src/Votolab/RCPXBundle/Controller/HomeController.php
@@ -8,6 +8,11 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
+        $user = $this->getUser();
+        if (!empty($user)) {
+            return $this->redirect($this->generateUrl('votolab_elections'));
+        }
+        
         return $this->render('RCPXBundle:Home:index.html.twig');
     }
 }
