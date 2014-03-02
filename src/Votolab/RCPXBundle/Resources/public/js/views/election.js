9bc8999a2a439e9ec76c90cdb8d40b5090c34f19
Alberto López <x@albertolopez.cc>
2014-03-02 21:50:39 +0100

    submit vote

--- src/Votolab/RCPXBundle/Resources/public/js/views/election.js
+++ src/Votolab/RCPXBundle/Resources/public/js/views/election.js
@@ -5,7 +5,30 @@ $(function () {
             $(this).siblings('.br-current-rating').addClass('br-current-rating-active');
         }
     });
-    $("button#submitVote").validate({
-
+    $("button#submitVote").click(function () {
+        var valid = true;
+        $.each($(this).parents('form').find('[data-rating]'), function () {
+            var criterion = $(this).parent('.form-group');
+            if (!$(this).val()) {
+                criterion.addClass('has-error');
+                criterion.find('.input-error').removeClass('hide');
+                valid = false;
+            } else {
+                criterion.find('.input-error').addClass('hide');
+            }
+        })
+        if (valid === true) {
+            var candidate = $(this).parents('[data-candidate]').data('candidate');
+            var ratings = $(this).parents('[data-ratings]').data('rating');
+            console.log( $( this ).parents('form').serialize() );
+            $.ajax({
+                type: "POST",
+                url: Routing.generate('votolab_vote', {slug: $('[data-election]').data('election')}),
+                data: {
+                    candidate: candidate,
+                    ratings: ratings
+                }
+            })
+        }
     });
 });
