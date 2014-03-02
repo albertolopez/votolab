d5ee9bd33995832d3ea704b372382b5dce1d6a4b
Alberto López <x@albertolopez.cc>
2014-03-02 15:58:12 +0100

    validate vote

--- src/Votolab/RCPXBundle/Resources/public/js/views/election.js
+++ src/Votolab/RCPXBundle/Resources/public/js/views/election.js
@@ -2,6 +2,10 @@ $(function () {
     $('[data-rating]').barrating('show', {
         showSelectedRating: true,
         onSelect: function (value, text) {
+            $(this).siblings('.br-current-rating').addClass('br-current-rating-active');
         }
+    });
+    $("button#submitVote").validate({
+
     });
 });
