62a2c28b013994bdb0b0fcb1e5b53e9c19c58446
Alberto López <x@albertolopez.cc>
2014-03-24 09:31:20 +0100

    add advanced properties to tinymce

--- src/Votolab/RCPXBundle/Resources/public/js/views/admin.js
+++ src/Votolab/RCPXBundle/Resources/public/js/views/admin.js
@@ -1,6 +1,11 @@
 tinymce.init({
     selector: "textarea",
     height: 300,
-    content_css : tinymce_stylesheet + ",http://fonts.googleapis.com/css?family=Open+Sans"
-
+    content_css : tinymce_stylesheet + ",http://fonts.googleapis.com/css?family=Open+Sans",
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
 });
